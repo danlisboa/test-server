2faffd7a3d155f3870d22000dcf01304f5e7fa9f
Daniel <daniellisboa.me@gmail.com>
2022-04-06 12:46:18 -0300

    change commands

--- init.sh
+++ init.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 echo "Starting API Hello Server"
-cd /home/ubuntu
-sudo apt update && apt upgrade
-mkdir projects
-cd projects/
-git clone https://github.com/danlisboa/test-server.git
-cd test-server/
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
-source ~/.bashrc
-nvm install node --lts
-npm install
-npm install pm2@latest -g
 pm2 start npm --name "API" -- start
