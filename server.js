d175c34492ea1458c697ecec797746929f5dbd6d
Daniel <daniellisboa.me@gmail.com>
2022-04-09 17:48:10 -0300

    new phrase

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world with express')
+  res.send('Deu certo Celinha !!!')
 })
 
 app.listen(3000)
