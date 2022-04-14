b344d47994e136e149820efb71832e180d96826a
Daniel <daniellisboa.me@gmail.com>
2022-04-14 00:40:39 -0300

    test build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('build na esteira...')
+  res.send('build na esteira....')
 })
 
 app.listen(3000)
