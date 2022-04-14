4d3b0e2fd2d3eb2b63271e87886ade7f5e3543b8
Daniel <daniellisboa.me@gmail.com>
2022-04-13 23:25:18 -0300

    change init

--- init.sh
+++ init.sh
@@ -2,19 +2,9 @@
 echo "Starting API Hello Server"
 set -x #echo on
 
-sudo snap install docker
-sudo snap status docker #check the status
-sudo snap start docker # start the service
-
-sudo addgroup --system docker
-sudo adduser ubuntu docker
-sudo usermod -a -G docker ubuntu
-docker -v
-docker info
-
 sudo service docker start
 
-docker kill $(docker ps -q)
+sudo docker kill $(docker ps -q)
 
 sudo docker build . -t test-app
 
