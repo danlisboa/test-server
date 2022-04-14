998b9a01eb4b39f57312414895673feb7a458795
Daniel <daniellisboa.me@gmail.com>
2022-04-14 05:01:47 -0300

    esteira funcionando

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('api changed....')
+  res.send('esteira funcionando ....')
 })
 
 app.listen(3000)
