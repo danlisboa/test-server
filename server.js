68e99ad39265e92eea8d20bc0156e748fccd6663
Daniel <daniellisboa.me@gmail.com>
2022-04-14 23:04:34 -0300

    change message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('esteira funcionando e replicando ....')
+  res.send('esteira funcionando e replicando na api ....')
 })
 
 app.listen(3000)
